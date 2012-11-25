142d16a8eb5f72c982647424c2f002af58f2b167
optiklab <byslash@gmail.com>
2012-11-25 15:51:55 +0400

    All code

---
+++ String Replacer/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace String_Replacer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.txbPath = new System.Windows.Forms.TextBox();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.lblFrom = new System.Windows.Forms.Label();
+            this.lblTo = new System.Windows.Forms.Label();
+            this.txbReplaceFrom = new System.Windows.Forms.TextBox();
+            this.txbReplaceTo = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnOpenFolder = new System.Windows.Forms.Button();
+            this.txbMask = new System.Windows.Forms.TextBox();
+            this.lblMask = new System.Windows.Forms.Label();
+            this.txbLog = new System.Windows.Forms.TextBox();
+            this.lblLink = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txbPath
+            // 
+            this.txbPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txbPath.Location = new System.Drawing.Point(142, 17);
+            this.txbPath.Multiline = true;
+            this.txbPath.Name = "txbPath";
+            this.txbPath.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txbPath.Size = new System.Drawing.Size(402, 86);
+            this.txbPath.TabIndex = 0;
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(60, 19);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(75, 37);
+            this.btnOpenFile.TabIndex = 1;
+            this.btnOpenFile.Text = "Search in File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // lblFrom
+            // 
+            this.lblFrom.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblFrom.AutoSize = true;
+            this.lblFrom.Location = new System.Drawing.Point(141, 148);
+            this.lblFrom.Name = "lblFrom";
+            this.lblFrom.Size = new System.Drawing.Size(72, 13);
+            this.lblFrom.TabIndex = 2;
+            this.lblFrom.Text = "Replace this: ";
+            // 
+            // lblTo
+            // 
+            this.lblTo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTo.AutoSize = true;
+            this.lblTo.Location = new System.Drawing.Point(149, 220);
+            this.lblTo.Name = "lblTo";
+            this.lblTo.Size = new System.Drawing.Size(64, 13);
+            this.lblTo.TabIndex = 3;
+            this.lblTo.Text = "Replace by:";
+            // 
+            // txbReplaceFrom
+            // 
+            this.txbReplaceFrom.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txbReplaceFrom.Location = new System.Drawing.Point(215, 145);
+            this.txbReplaceFrom.Multiline = true;
+            this.txbReplaceFrom.Name = "txbReplaceFrom";
+            this.txbReplaceFrom.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txbReplaceFrom.Size = new System.Drawing.Size(329, 64);
+            this.txbReplaceFrom.TabIndex = 4;
+            // 
+            // txbReplaceTo
+            // 
+            this.txbReplaceTo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txbReplaceTo.Location = new System.Drawing.Point(215, 217);
+            this.txbReplaceTo.Multiline = true;
+            this.txbReplaceTo.Name = "txbReplaceTo";
+            this.txbReplaceTo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txbReplaceTo.Size = new System.Drawing.Size(329, 68);
+            this.txbReplaceTo.TabIndex = 5;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(60, 118);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(73, 40);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnOpenFolder
+            // 
+            this.btnOpenFolder.Location = new System.Drawing.Point(59, 62);
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(75, 37);
+            this.btnOpenFolder.TabIndex = 7;
+            this.btnOpenFolder.Text = "Search in Folder";
+            this.btnOpenFolder.UseVisualStyleBackColor = true;
+            this.btnOpenFolder.Click += new System.EventHandler(this.btnOpenFolder_Click);
+            // 
+            // txbMask
+            // 
+            this.txbMask.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txbMask.Location = new System.Drawing.Point(215, 118);
+            this.txbMask.Name = "txbMask";
+            this.txbMask.Size = new System.Drawing.Size(329, 20);
+            this.txbMask.TabIndex = 8;
+            // 
+            // lblMask
+            // 
+            this.lblMask.AutoSize = true;
+            this.lblMask.Location = new System.Drawing.Point(144, 121);
+            this.lblMask.Name = "lblMask";
+            this.lblMask.Size = new System.Drawing.Size(69, 13);
+            this.lblMask.TabIndex = 9;
+            this.lblMask.Text = "Mask of files:";
+            // 
+            // txbLog
+            // 
+            this.txbLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txbLog.Location = new System.Drawing.Point(60, 298);
+            this.txbLog.Multiline = true;
+            this.txbLog.Name = "txbLog";
+            this.txbLog.ReadOnly = true;
+            this.txbLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txbLog.Size = new System.Drawing.Size(484, 80);
+            this.txbLog.TabIndex = 10;
+            // 
+            // lblLink
+            // 
+            this.lblLink.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblLink.AutoSize = true;
+            this.lblLink.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblLink.Location = new System.Drawing.Point(327, 383);
+            this.lblLink.Name = "lblLink";
+            this.lblLink.Size = new System.Drawing.Size(219, 13);
+            this.lblLink.TabIndex = 11;
+            this.lblLink.Text = "Made by OptikLab © www.optiklab.ru";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(9, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(41, 35);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(9, 174);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(41, 35);
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.Location = new System.Drawing.Point(9, 321);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(41, 35);
+            this.pictureBox3.TabIndex = 14;
+            this.pictureBox3.TabStop = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 401);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblLink);
+            this.Controls.Add(this.txbLog);
+            this.Controls.Add(this.lblMask);
+            this.Controls.Add(this.txbMask);
+            this.Controls.Add(this.btnOpenFolder);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txbReplaceTo);
+            this.Controls.Add(this.txbReplaceFrom);
+            this.Controls.Add(this.lblTo);
+            this.Controls.Add(this.lblFrom);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.txbPath);
+            this.Name = "frmMain";
+            this.Text = "tReplace Tool";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbPath;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Label lblFrom;
+        private System.Windows.Forms.Label lblTo;
+        private System.Windows.Forms.TextBox txbReplaceFrom;
+        private System.Windows.Forms.TextBox txbReplaceTo;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnOpenFolder;
+        private System.Windows.Forms.TextBox txbMask;
+        private System.Windows.Forms.Label lblMask;
+        private System.Windows.Forms.TextBox txbLog;
+        private System.Windows.Forms.Label lblLink;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ String Replacer/Form1.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+namespace String_Replacer
+{
+    #region Description
+    ///<summary>
+    /// Description
+    ///</summary>
+    public partial class frmMain : Form
+    {
+        #region Private Member Variables
+        private bool IsFolder;
+        #endregion
+
+        #region Private Methods
+        //Open File button handler
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.Filter = "Any type|*.*";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    txbPath.Text = ofd.FileName;
+                    IsFolder = false;
+                }
+            }
+            catch (OutOfMemoryException)
+            {
+                MessageBox.Show("Application can't open files!");
+            }
+            ChangeView();
+        }
+
+        //Open Folder button handler
+        private void btnOpenFolder_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FolderBrowserDialog fbd = new FolderBrowserDialog();
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    txbPath.Text = fbd.SelectedPath;
+                    IsFolder = true;
+                }
+            }
+            catch (OutOfMemoryException)
+            {
+                MessageBox.Show("Application can't open folders!");
+            }
+            ChangeView();
+        }
+
+        //Start button handler
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (ControlsValidator() == true)
+            {
+                Replacer();
+                pictureBox1.Visible = false;
+                pictureBox2.Visible = false;
+                pictureBox3.Visible = true;
+            }            
+        }
+
+        //function
+        private bool IsFolderExists(string Path)
+        {
+            try
+            {
+                DirectoryInfo di = new DirectoryInfo(Path);
+                if (di.Exists == true)
+                    return true;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Can't get access to directory!");
+            }
+            return false;
+        }
+
+        //Function check for existing of file in Path
+        private bool IsFileExists(string Path)
+        {
+            try
+            {
+                FileInfo fi = new FileInfo(Path);
+                if (fi.Exists == true)
+                    return true;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Can't get access to file!");
+            }
+            return false;
+        }
+
+        //Method constraint mask from string entered in Mask TextBox
+        private string MaskConstraint(string Mask)
+        {
+            try
+            {
+                // Replace . to \.
+                Mask = Mask.Replace(".", @"\." /* (".", "\\.") */);
+                // Replace ? to .
+                Mask = Mask.Replace("?", ".");
+                // Replace * to .*
+                Mask = Mask.Replace("*", ".*");
+                // We need to find string exact to mask:
+                Mask = "^" + Mask + "$";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Mask is wrong!");
+                return String.Empty;
+            }
+            return Mask;
+        }
+
+        //Method changes view of controls step by step to help User
+        private void ChangeView()
+        {
+            if (txbPath.Text == String.Empty)
+            {
+                txbReplaceTo.Enabled = false;
+                txbReplaceFrom.Enabled = false;
+                txbMask.Enabled = false;
+                btnStart.Enabled = false;
+                pictureBox1.Visible = true;
+                pictureBox2.Visible = false;
+                pictureBox3.Visible = false;
+            }
+            else
+            {
+                txbReplaceTo.Enabled = true;
+                txbReplaceFrom.Enabled = true;
+                txbMask.Enabled = true;
+                btnStart.Enabled = true;
+                pictureBox1.Visible = false;
+                pictureBox2.Visible = true;
+                pictureBox3.Visible = false;
+            }
+
+            if (IsFolder == true)
+                txbMask.Enabled = true;
+            else
+                txbMask.Enabled = false;
+        }
+
+        //Function check for file paths in Text Boxes
+        private bool ControlsValidator()
+        {
+            if(txbReplaceFrom.Text != String.Empty && txbPath.Text != String.Empty)
+            {
+                if(IsFolder == true)
+                {
+                    if(txbMask.Text != String.Empty && IsFolderExists(txbPath.Text) == true)
+                        return true;
+                }
+                else
+                {
+                    if (IsFileExists(txbPath.Text) == true)
+                        return true;
+                }
+            }
+            return false;
+        }
+        #endregion
+
+        #region Constructor
+        public frmMain()
+        {
+            InitializeComponent();
+            IsFolder = false;
+            txbReplaceTo.Enabled = false;
+            txbReplaceFrom.Enabled = false;
+            txbMask.Enabled = false;
+            btnStart.Enabled = false;
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+        }
+        #endregion
+
+        #region Public methods
+
+        //Main function of replacing
+        public void Replacer()
+        {
+            string Mask = MaskConstraint(txbMask.Text);
+            string TextToReplace = txbReplaceFrom.Text;
+            string TextToAppend = txbReplaceTo.Text;
+
+            //create regular expression object from Mask
+            Regex regMask = new Regex(Mask, RegexOptions.IgnoreCase);
+            //escape from special symbols
+            TextToReplace = Regex.Escape(TextToReplace);
+            //create regular expression object from Text
+            Regex regText = new Regex(TextToReplace, RegexOptions.Multiline);
+            txbLog.Text = String.Empty;
+            //if search in folder: need to find all folders and files under it 
+            if (IsFolder == true)
+            {
+                InFolderReplacer(txbPath.Text, regText, regMask, TextToAppend);
+            }
+            else
+            {
+                InFileReplacer(txbPath.Text, regText, TextToAppend);
+            }
+        }
+
+        //Function find directory and text in files under this directory
+        public void InFolderReplacer(string Path, Regex regText, Regex regMask, string TextToAppend)
+        {
+            ulong Matches = 0;
+            try
+            {
+                DirectoryInfo di = new DirectoryInfo(Path);
+                Matches = FindTextInFiles(di, regText, regMask, TextToAppend);
+            }
+            catch (OutOfMemoryException)
+            {
+                MessageBox.Show("You don't have enough memory for continue work!");
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("Please fix folder path!");
+            }
+            catch (System.Security.SecurityException)
+            {
+                MessageBox.Show("Application doesn't have enough access rights!");
+            }
+            catch (PathTooLongException)
+            {
+                MessageBox.Show("Please input less path!");
+            }
+            //show status in Log textbox
+            txbLog.Text = Matches.ToString() + " files found!" + Environment.NewLine + txbLog.Text;
+        }
+
+
+
+        // Recurrence function  find text in files of THIS folder 
+        // and under all subfolders with recursion call
+        public ulong FindTextInFiles(DirectoryInfo di, Regex regText, Regex regMask, string TextToAppend)
+        {
+            ulong CountOfMatchFiles = 0;
+            FileInfo[] fi = null;
+            try  // Get List of Files
+            {
+                fi = di.GetFiles();
+                //Search in list of files
+                foreach (FileInfo f in fi)
+                {
+                    if (regMask.IsMatch(f.Name)) //in files which is appropriate to mask
+                    {
+                        ++CountOfMatchFiles;
+                        InFileReplacer(di.FullName + @"\" + f.Name, regText, TextToAppend);
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Can't get access to file!");
+                return CountOfMatchFiles;
+            }
+            //search in subfolders 
+            try
+            {
+                DirectoryInfo[] diSub = di.GetDirectories();  // list of subfolders
+                foreach (DirectoryInfo diSubDir in diSub) // Recursion for every subfolder
+                    CountOfMatchFiles += FindTextInFiles(diSubDir, regText, regMask, TextToAppend);
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Can't search in subfolders!");
+                return CountOfMatchFiles;
+            }
+            return CountOfMatchFiles;
+        }
+
+        //Replace text in file
+        public void InFileReplacer(string Path, Regex regText, string TextToAppend)
+        {
+            string Result = String.Empty;
+            string Content = String.Empty;
+            //read
+            try
+            {
+                StreamReader sr = new StreamReader(Path, Encoding.Default);
+                Content = sr.ReadToEnd();
+                sr.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Can't read file!");
+            }
+            //replace
+            try
+            {
+                Result = regText.Replace(Content, TextToAppend);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Can't replace text in file!");
+            }
+            if (Result == Content)
+                txbLog.Text += "+ Text is not found in " + Path + Environment.NewLine;
+            else
+            {
+                //write
+                try
+                {
+                    StreamWriter writetofile = new StreamWriter(Path, false, Encoding.Default, Result.Length);
+                    writetofile.Write(Result);
+                    writetofile.Close();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Can't write text in file!");
+                }
+                txbLog.Text += "+ Text replaced in: " + Path + Environment.NewLine;
+            }
+        }
+        #endregion
+    }
+    #endregion
+}
